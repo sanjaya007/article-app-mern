298d067f21da60e0732b21eaa7282e48a7a66279
Sanjaya Paudel <paudelsanjaya702@gmail.com>
2023-01-31 11:39:52 +0545

    mern app added

---
+++ client/src/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+
+const About = () => {
+  return (
+    <>
+      <div>
+        <h1>About</h1>
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
